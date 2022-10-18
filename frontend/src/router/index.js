e1bdb481288b4e765cc9a9d2319be6d6ceeb416e
AyumiizZ <korrawitc@gmail.com>
2022-10-18 13:03:07 +0700

    :sparkles: add coming soon page for unfinished

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import SignInView from "../views/SignInView.vue";
+import HomeView from "@/views/HomeView.vue";
+import SignInView from "@/views/SignInView.vue";
+import ComingSoonView from "@/views/ComingSoonView.vue";
 
 Vue.use(VueRouter);
 
@@ -24,6 +25,36 @@ const routes = [
     path: "/signin",
     name: "signin",
     component: SignInView,
+  },
+  {
+    path: "/applicant",
+    name: "applicant",
+    component: ComingSoonView,
+  },
+  {
+    path: "/responsible",
+    name: "responsible",
+    component: ComingSoonView,
+  },
+  {
+    path: "/approver",
+    name: "approver",
+    component: ComingSoonView,
+  },
+  {
+    path: "/domain-regis",
+    name: "domain-regis",
+    component: ComingSoonView,
+  },
+  {
+    path: "/host-req",
+    name: "host-req",
+    component: ComingSoonView,
+  },
+  {
+    path: "/host-regis",
+    name: "host-regis",
+    component: ComingSoonView,
   },
 ];
 
